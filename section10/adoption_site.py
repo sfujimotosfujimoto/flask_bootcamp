533e7f34e5af753ff4654f43a51e87e12a8ac500
S Fujimoto <unicoshun@gmail.com>
2018-06-22 22:15:07 +0900

    ~88

--- section10/adoption_site.py
+++ section10/adoption_site.py
@@ -1,5 +1,5 @@
 import os
-# from forms import AddForm, DelForm
+from forms import AddForm, DelForm
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -31,6 +31,55 @@ class Puppy(db.Model):
 
     def __repr__(self):
         return f"Puppy name: {self.name}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# === VIEW FUNCTIONS
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_pup():
+    form = AddForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+
+        new_pup = Puppy(name)
+        db.session.add(new_pup)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+
+    return render_template('add.html', form=form)
+
+
+@app.route('/list')
+def list_pup():
+    puppies = Puppy.query.all()
+    return render_template('list.html', puppies=puppies)
+
+
+@app.route('/delete', methods=['GET', 'POST'])
+def del_pup():
+
+    form = DelForm()
+
+    if form.validate_on_submit():
+
+        id = form.id.data
+        pup = Puppy.query.get(id)
+        db.session.delete(pup)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+    return render_template('delete.html', form=form)
 
 
 if __name__ == '__main__':
