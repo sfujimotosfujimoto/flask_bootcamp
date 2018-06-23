e30110472a769174923f132576adb1d783f5e01d
S Fujimoto <unicoshun@gmail.com>
2018-06-23 23:56:33 +0900

    ~91

--- section10/adoption_site.py
+++ section10/adoption_site.py
@@ -1,47 +1,77 @@
 import os
-from forms import AddForm, DelForm
+from forms import AddForm, DelForm, AddOwnerForm
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 app.config['SECRET_KEY'] = 'mysecret'
 
 # === SQL DATABASE SECTION ===============================
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'database.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 Migrate(app, db)
 
 
-# =========================================
-
-# === MOMDELS ===================================
+# === MODELS ===================================
 
 class Puppy(db.Model):
     __tablename__ = 'puppies'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
+    owner = db.relationship('Owner', backref='puppy', uselist=False)
 
     def __init__(self, name):
         self.name = name
 
     def __repr__(self):
-        return f"Puppy name: {self.name}"
+        if self.owner:
+            return f"Puppy name is {self.name} and owner is {self.owner.name}"
+        else:
+            return f"Puppy name: {self.name} and no owner assigned yet!"
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+class Owner(db.Model):
+    __tablename__ = 'owners'
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    puppy_id = db.Column(db.Integer, db.ForeignKey('puppies.id'))
+
+    def __init__(self, name, puppy_id):
+        self.name = name
+        self.puppy_id = puppy_id
+
+    def __repr__(self):
+        return f"Owner Name: {self.name}"
 
 # === VIEW FUNCTIONS
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+
+@app.route('/add_owner', methods=['GET', 'POST'])
+def add_owner():
+    form = AddOwnerForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        pup_id = form.pup_id.data
+
+        new_owner = Owner(name, pup_id)
+        db.session.add(new_owner)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+    return render_template('add_owner.html', form=form)
 
 
 @app.route('/add', methods=['GET', 'POST'])
@@ -68,11 +98,9 @@ def list_pup():
 
 @app.route('/delete', methods=['GET', 'POST'])
 def del_pup():
-
     form = DelForm()
 
     if form.validate_on_submit():
-
         id = form.id.data
         pup = Puppy.query.get(id)
         db.session.delete(pup)
@@ -84,3 +112,4 @@ def del_pup():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # app.run(host='0.0.0.0')
