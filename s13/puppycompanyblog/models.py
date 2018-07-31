c1b531828bf7948e8e602493ad2fa35d447f1939
S Fujimoto <unicoshun@gmail.com>
2018-07-31 10:13:38 +0900

    ~117

--- s13/puppycompanyblog/models.py
+++ s13/puppycompanyblog/models.py
@@ -15,7 +15,7 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     profile_image = db.Column(
-        db.String(64), nullable=False, default='default_profile.png')
+        db.String(64), nullable=False, default='default_profile.jpg')
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
--- s13/puppycompanyblog/users/forms.py
+++ s13/puppycompanyblog/users/forms.py
@@ -17,11 +17,11 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
-    username = StringField('UserName', validators=[DataRequired(), Email()])
+    username = StringField('UserName', validators=[DataRequired()])
     password = PasswordField('Password',
                              validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords must match!')])
     pass_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
-    submit = SubmitField('Register')
+    submit = SubmitField('Register!')
 
     def check_email(self, field):
         if User.query.filter_by(email=field.data).first():
